41991e53477fdda50caa8773426f424ab8d5e1ef
Claus Bo Madsen <claus.bo.madsen@icloud.com>
2022-09-10 09:04:06 +0200

    open conflicts in vscode

--- gitty.py
+++ gitty.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 # todo:
-    # rebase-onto
     # checkout-local
     # diff
     # clean-up branched
     # search feature branch
     
-from tabnanny import check
 import click
-from subprocess import check_output, check_call
+from subprocess import STDOUT, CalledProcessError, check_output, check_call
 
 def get_current_branch():
     return check_output(["git", "branch", "--show-current"], text=True).rstrip("\n")
@@ -17,11 +15,41 @@ def get_current_branch():
 def git_checkout(branch):
     check_call(["git", "checkout", branch])
 
-def git_pull():
-    check_call(["git", "pull"])   
+def git_pull() -> bool:
+    try:
+        check_output(["git", "pull"], text=True, stderr=STDOUT)   
+    except CalledProcessError as e:
+        output = str(e.output)
+        if output.startswith("There is no tracking information for the current branch."):
+            print("No tracking information for the current branch.")
+            return False
+        else:
+            raise e
+    else:
+        return True
 
-def git_rebase(branch):
-    check_call(["git", "rebase", branch])
+def git_rebase(branch) -> bool:
+    try:
+        check_output(["git", "rebase", branch], text=True, stderr=STDOUT)
+    except CalledProcessError as e:
+        output = str(e.output)
+        if "CONFLICT" in output:
+            for line in output.splitlines():
+                if "conflict in" in line:
+                    print(line)
+            return False
+        else:
+            raise e
+    else:
+        return True
+    
+def git_status() -> str:
+    return check_output(["git", "status"], text=True, stderr=STDOUT)
+
+
+def open_in_vscode(filename):
+    check_call(["code", filename])
+    
 
 @click.group("gitty")
 def gitty():
@@ -37,9 +65,11 @@ def rebase(branch, allow_master, no_pull):
         git_checkout(branch)
         git_pull()
     git_checkout(source_branch)
-    git_rebase(branch)
+    if not git_rebase(branch):
+        for line in git_status().splitlines():
+            if "modified" in line:
+                open_in_vscode(line.split()[-1])
+                
         
-    print(f"git rebase {get_current_branch()} {branch}")
-
 if __name__ == "__main__":
     gitty()
